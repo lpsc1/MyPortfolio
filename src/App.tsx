9e61e9b994612a00e5c6a5e1f0f2296190f3892a
Lucas <lucascor_rtb@hotmail.com>
2022-08-01 18:24:05 -0400

    Excluding libraires not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Dashboard/Header";
 import { ScreenContextProvider } from "./context";
 import { GlobalStyle } from "./styles/global";
 
